a7c8cd31253fe086e3cdc3df21ff96014b0bc53d
DoctorTone <tony@drt-software.com>
2025-09-09 15:36:16 +0100

    Added animations

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -1,11 +1,17 @@
-import { Stage, useGLTF } from "@react-three/drei";
+import { useEffect } from "react";
+import { Stage, useGLTF, useAnimations } from "@react-three/drei";
 
 const Scene = () => {
-  const { scene } = useGLTF("./models/dinosaur.glb");
+  const { scene, animations } = useGLTF("./models/dinosaur.glb");
+  const { ref, actions } = useAnimations(animations);
+
+  useEffect(() => {
+    actions?.Animation?.play();
+  }, []);
 
   return (
     <Stage adjustCamera={false} shadows="contact">
-      <primitive object={scene} />
+      <primitive ref={ref} object={scene} />
     </Stage>
   );
 };
