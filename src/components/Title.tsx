44927b34f0877a8062511b35158dc2cc05dde291
DoctorTone <tony@drt-software.com>
2025-09-24 00:17:12 +0100

    Updated title text colour

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -3,7 +3,13 @@ import Typography from "@mui/material/Typography";
 const Title = () => {
   return (
     <div id="title" className="panel">
-      <Typography variant="h3" sx={{ fontStyle: "italic", color: "#555" }}>
+      <Typography
+        variant="h3"
+        sx={{
+          fontStyle: "italic",
+          color: "#555",
+        }}
+      >
         Pterodactylus
       </Typography>
     </div>
